292bea4f1f0d9f7e21cad08a6f5c57aa94749e19
Eustas <luffyxkx@gmail.com>
2024-05-25 12:30:29 -0500

    Last changes 25-05

--- js/contacto.js
+++ js/contacto.js
@@ -1,31 +1,31 @@
-const  el = document.getElementsByClassName('picture-contact')
-const height = el.clientHeight
-const width = el.clientWidth
+// const  el = document.getElementsByClassName('picture-contact')
+// const height = el.clientHeight
+// const width = el.clientWidth
 
-el.addEventListener('mousemove', (evt) => {
-    const {layerX, layerY} = evt
+// el.addEventListener('mousemove', (evt) => {
+//     const {layerX, layerY} = evt
 
-    const yRotation = (
-        (layerX - width / 2) / width
-    ) * 20
-    const xRotation = (
-        (layerY - height / 2) / height
-    ) * 20
+//     const yRotation = (
+//         (layerX - width / 2) / width
+//     ) * 20
+//     const xRotation = (
+//         (layerY - height / 2) / height
+//     ) * 20
 
-    const string = `
-        perspective(500px)
-        scale(1.1)
-        rotateX(${xRotation}deg)
-        rotateY(${yRotation}deg)`
+//     const string = `
+//         perspective(500px)
+//         scale(1.1)
+//         rotateX(${xRotation}deg)
+//         rotateY(${yRotation}deg)`
     
-    el.style.transform = string
+//     el.style.transform = string
 
-})
+// })
 
-el.addEventListener('mouseout', () => {
-    el.style.transform = `
-        perspective(500px)
-        scale(1)
-        rotateX(0)
-        rotateY(0)`
-})
+// el.addEventListener('mouseout', () => {
+//     el.style.transform = `
+//         perspective(500px)
+//         scale(1)
+//         rotateX(0)
+//         rotateY(0)`
+// })

a8df07692c4d6aa61bba4c9464aa703a0b06e6b7
Eustas <luffyxkx@gmail.com>
2024-05-28 10:45:19 -0500

    Last changes(presentation)

--- js/contacto.js
+++ js/contacto.js
@@ -1,6 +1,15 @@
-const input = document.querySelector("#phone");
-  window.intlTelInput(input, {
-    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@23.0.10/build/js/utils.js",
-  });
+const phoneInputField = document.querySelector("#phone");
 
+window.intlTelInput(phoneInputField, {
+  initialCountry: "auto",
+  separateDialCode: true,
+  geoIpLookup: callback => {
+    fetch("https://ipapi.co/json")
+      .then(res => res.json())
+      .then(data => callback(data.country_code))
+      .catch(() => callback("us"));
+  },
+  utilsScript:
+    "https://cdn.jsdelivr.net/npm/intl-tel-input@23.0.10/build/js/utils.js",
+});
 

edebba718947c27fa2394eceec8e6d596f1d9ddb
Eustas <luffyxkx@gmail.com>
2024-06-05 17:11:19 -0500

    Last changes 05 - 06

--- js/contacto.js
+++ js/contacto.js
@@ -1,4 +1,5 @@
 const phoneInputField = document.querySelector("#phone");
+const btn = document.getElementById('btn');
 
 window.intlTelInput(phoneInputField, {
   initialCountry: "auto",
@@ -11,5 +12,24 @@ window.intlTelInput(phoneInputField, {
   },
   utilsScript:
     "https://cdn.jsdelivr.net/npm/intl-tel-input@23.0.10/build/js/utils.js",
+});
+
+document.getElementById('lookup')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_v0skm3o';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Mensaje enviado';
+      alert('El mensaje fue enviado!');
+    }, (err) => {
+      btn.value = 'El mensaje no fue enviado';
+      alert(JSON.stringify(err));
+    });
 });
 

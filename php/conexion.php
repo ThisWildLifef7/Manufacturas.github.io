a72550e9216c31687276ac159e31c56169fb78be
Eustas <luffyxkx@gmail.com>
2024-06-07 12:22:30 -0500

    Last changes 07 - 06

---
+++ php/conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+    // conexionphp();
+    // function conexionphp(){
+    //     $server = 'localhost';
+    //     $user = 'root';
+    //     $pass = 'onepiecef1';
+    //     $db = 'manufacturas';
+    //     $conectar = mysqli_connect($server, $user, $pass, $db)or die ("Error en la conexión");
+    // }
+
+?>
